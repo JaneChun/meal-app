72a67814ae6b6a3d3e1e7bb943d9bae23e927aa2
JaneChun <janechun22@gmail.com>
2025-01-07 16:15:42 +0900

    React Navigation 구성

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,17 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 export default function Index() {
 	return (
 		<>
-			<StatusBar style='light' />
-			<CategoriesScreen />
+			<StatusBar style='dark' />
+			<Stack.Navigator>
+				<Stack.Screen name='Meals Categories' component={CategoriesScreen} />
+			</Stack.Navigator>
 		</>
 	);
 }
