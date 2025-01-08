d8d03f8046f90652c316e1b3e0454a3347ca6e7b
JaneChun <janechun22@gmail.com>
2025-01-08 14:29:39 +0900

    중첩 네비게이션 적용(stack + drawer)

--- app/index.tsx
+++ app/index.tsx
@@ -5,70 +5,59 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import MealsOverviewScreen from '../screens/MealsOverviewScreen';
 import MealDetilScreen from '../screens/MealDetailScreen';
-import { AntDesign, Entypo, Ionicons } from '@expo/vector-icons';
+import FavoritesScreen from '../screens/FavoritesScreen';
+import { AntDesign } from '@expo/vector-icons';
 
-// const Stack = createNativeStackNavigator();
-
+const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+	return (
+		<Drawer.Navigator
+			screenOptions={{
+				drawerActiveBackgroundColor: '#FFE2E2',
+				drawerActiveTintColor: '#86A788',
+				headerStyle: { backgroundColor: '#86A788' },
+				drawerStyle: { backgroundColor: '#FFFDEC' },
+			}}
+		>
+			<Drawer.Screen
+				name='Meals Categories'
+				component={CategoriesScreen}
+				options={{
+					title: 'All Categories',
+					headerTintColor: 'white',
+					drawerLabel: 'Home',
+					drawerIcon: ({ color, size }) => (
+						<AntDesign name='home' color={color} size={size} />
+					),
+				}}
+			/>
+			<Drawer.Screen
+				name='Favorites'
+				component={FavoritesScreen}
+				options={{
+					headerTintColor: 'white',
+					drawerIcon: ({ color, size }) => (
+						<AntDesign name='star' color={color} size={size} />
+					),
+				}}
+			/>
+		</Drawer.Navigator>
+	);
+}
 
 export default function Index() {
 	return (
 		<>
 			<StatusBar style='dark' />
-			<Drawer.Navigator
-				screenOptions={{
-					drawerActiveBackgroundColor: '#FFE2E2',
-					drawerActiveTintColor: '#86A788',
-					headerStyle: { backgroundColor: '#86A788' },
-					drawerStyle: { backgroundColor: '#FFFDEC' },
-				}}
-			>
-				<Drawer.Screen
-					name='Meals Categories'
-					component={CategoriesScreen}
-					options={{
-						title: 'All Categories',
-						headerTitleAlign: 'center',
-						headerTintColor: 'white',
-						drawerLabel: 'Home',
-						drawerIcon: ({ color, size }) => (
-							<AntDesign name='home' color={color} size={size} />
-						),
-					}}
-				/>
-				<Drawer.Screen
-					name='Meals Overview'
-					component={MealsOverviewScreen}
-					options={{
-						headerTitleAlign: 'center',
-						drawerIcon: ({ color, size }) => (
-							<Ionicons name='fast-food' color={color} size={size} />
-						),
-					}}
-				/>
-				<Drawer.Screen
-					name='Meal Detail'
-					component={MealDetilScreen}
-					options={{
-						headerStyle: { backgroundColor: '#FFB0B0' },
-						headerTitleAlign: 'center',
-						headerTintColor: 'white',
-						drawerIcon: ({ color, size }) => (
-							<Entypo name='list' color={color} size={size} />
-						),
-					}}
-				/>
-			</Drawer.Navigator>
 
-			{/* <Stack.Navigator>
+			<Stack.Navigator>
 				<Stack.Screen
 					name='Meals Categories'
-					component={CategoriesScreen}
+					component={DrawerNavigator}
 					options={{
-						title: 'All Categories',
-						headerStyle: { backgroundColor: '#86A788' },
-						headerTitleAlign: 'center',
-						headerTintColor: 'white',
+						headerShown: false,
 					}}
 				/>
 				<Stack.Screen
@@ -91,7 +80,7 @@ export default function Index() {
 						headerTintColor: 'white',
 					}}
 				/>
-			</Stack.Navigator> */}
+			</Stack.Navigator>
 		</>
 	);
 }
